1983026fbfb79ff5c380073cfdf061b48c59fc13
kumamoto-city <pokesma@yahoo.co.jp>
2014-02-10 07:07:07 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'hikari2013budget';
+OpenSpending.identifier = 'my_pokesma';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
