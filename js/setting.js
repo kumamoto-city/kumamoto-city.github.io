436437798ffcee07ec1c3f06bbce944af2208ff5
kumamoto-city <pokesma@yahoo.co.jp>
2014-02-09 20:29:30 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'suita_city_general_account_budget';
+OpenSpending.identifier = 'shiojiri_2013_yosan';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
