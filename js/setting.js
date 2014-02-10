7707e75e3e6f3cda73ee218a7bf8001d0ae014df
kumamoto-city <pokesma@yahoo.co.jp>
2014-02-10 16:22:25 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -19,7 +19,7 @@ OpenSpending.Styles.Cofog = {
 
   /* kumamoto cofog */
     
-   '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+   '1': { icon: 'icons/helping-others.svg', color: '#C75747', bcolor: '#935B3B' },
   '1-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
   '2': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
   '2-1': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
